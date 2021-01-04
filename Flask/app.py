fa3cfd39021e3a4d34e3a8fc19bc4a1168432718
d-khalili <dalton.dwyer@gmail.com>
2021-01-04 10:43:40 -0500

    starting test

--- Flask/app.py
+++ Flask/app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 
 def index():
-    return "Hello, World"
+    number = request.args.get("6")
+    return render_template('index.html', number=number)
 
 if __name__ == "__main__":
     app.run(debug = True)
