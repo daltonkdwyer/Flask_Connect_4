2618ddf00c808bbc0717dc21df4b47b91ff7ef3d
Dalton Dwyer <daltondwyer@macbook-pro-4.mynetworksettings.com>
2022-07-01 10:47:34 -0400

    CHANGING TO TESTING MODE

---
+++ Readme.md
@@ -0,0 +1,13 @@
+<!-- This Folder is live on Heroku
+
+It has two structures:
+1) Current Test structure
+    A) This is currently deployed
+
+
+2) Old Connect 4 app (which still has some bugs)
+    A) To deploy this, remove everything out of the 'ORIGINAL CONNECT FOUR' folder and put at the top level
+
+
+
+ -->
--- app.py
+++ app.py
@@ -1,32 +1,10 @@
 from flask import Flask, render_template, request
-import backend
 
 app = Flask(__name__)
 
-G = backend.Game()
-
-@app.route('/', methods=["GET", "POST"])
-def index():
-
-    game_won = G.play_game()
-
-    if game_won is not None:
-        if game_won[0] == 1:
-            win_set = game_won[1]
-            return render_template('endgame.html', player_1_list=G.B.player_1_set, player_2_list=G.B.player_2_set, win_set=win_set, possible_move_list=G.B.possible_move_list)
-        if game_won[0] == 3:
-            return "Game Drawn"
-
-    return render_template("index.html", player_1_list=G.B.player_1_set, player_2_list=G.B.player_2_set, possible_move_list=G.B.possible_move_list)
-
-@app.route('/endgame', methods=["GET", "POST"])
-def endgame():
-    if request.form.get("submit_button") == 'Restart Game':
-        G.reset()
-        return render_template("index.html", player_1_list=G.B.player_1_set, player_2_list=G.B.player_2_set, possible_move_list=G.B.possible_move_list)
-
-    return render_template("endgame.html")
-
+@app.route('/', methods=['GET', 'POST'])
+def on():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
